c223a3d374a73390a1793127fbf54dfd7822b71a
trongpham <phamvantrong7.qn@gmail.com>
2022-07-09 19:44:20 +0700

    Api part 1

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,25 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //activities/id
+        public async Task<ActionResult<Activity>> GetActivity(Guid id) => await _context.Activities.FindAsync(id);
+    }
+}
